497259b9e3a9b2f4344ae2e0ea7471b1a627f805
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-10-08 12:45:30 +0330

    Update block.go

--- internal/api/block.go
+++ internal/api/block.go
@@ -1,1 +1,41 @@
 package api
+
+import (
+	"time"
+
+	"github.com/Mohsen20031203/learn-gochain-core/internal/model"
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) CreatBlock(c *gin.Context) {
+
+	var block model.Block
+	err := c.BindJSON(&block)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request"})
+		return
+	}
+	block.Timestamp = time.Now()
+
+	if len(s.Chain) == 0 {
+		block.Index = 0
+		block.PrevHash = "0"
+	} else {
+		block.Index = int64(len(s.Chain))
+		block.PrevHash = s.Chain[len(s.Chain)-1].Hash
+	}
+
+	block.Mine(2)
+
+	s.Chain = append(s.Chain, block)
+
+	c.JSON(200, gin.H{
+		"message": "Block created successfully",
+		"block":   block,
+	})
+
+}
+
+func (s *Server) GetChain(c *gin.Context) {
+
+}
