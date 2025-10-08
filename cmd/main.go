71e303d11cf36fe777b961126e5d57c705dbfc5d
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-10-08 11:57:18 +0330

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Mohsen20031203/learn-gochain-core/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
+
+	_ = config
 }
