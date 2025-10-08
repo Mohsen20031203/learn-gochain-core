b6ed8f18e2f9027e21239376595372b13cd4d7fe
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-10-08 12:45:33 +0330

    Update model.go

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,12 +1,37 @@
 package model
 
-import "time"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strings"
+	"time"
+)
 
 type Block struct {
 	Index     int64
 	Timestamp time.Time
+	Data      string `json:"data"`
 	//Transactions []Transaction
 	PrevHash string
 	Hash     string
 	Nonce    int64
+}
+
+func (b Block) CalculateHash() string {
+	record := fmt.Sprintf("%d%s%s%d", b.Index, b.Timestamp.String(), b.PrevHash, b.Nonce)
+	hash := sha256.Sum256([]byte(record))
+	return hex.EncodeToString(hash[:])
+}
+
+func (b *Block) Mine(difficulty int) {
+	prefix := strings.Repeat("0", difficulty)
+	for {
+		hash := b.CalculateHash()
+		if strings.HasPrefix(hash, prefix) {
+			b.Hash = hash
+			break
+		}
+		b.Nonce++
+	}
 }
